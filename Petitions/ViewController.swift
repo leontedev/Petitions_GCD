b661b14ac83f4ee2f2c8a0a8f400575f32074f60
Mihail87 <leontemihai@gmail.com>
2019-09-16 11:38:59 +0300

    Moved Request Fetching data JSON Parsing to the Background thread using GCD

--- Petitions/ViewController.swift
+++ Petitions/ViewController.swift
@@ -30,14 +30,19 @@ class ViewController: UITableViewController {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                parse(data: data)
-                return
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self?.parse(data: data)
+                    return
+                }
             }
+            
+            self?.showError()
         }
         
-        showError()
+        
         
     }
     
@@ -68,16 +73,22 @@ class ViewController: UITableViewController {
         if let petitions = try? decoder.decode(Petitions.self, from: data) {
             self.petitions = petitions.results
             self.filteredPetitions = petitions.results
-            tableView.reloadData()
+            
+            // UI should be done back on the on the Main Thread
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
         }
     }
     
     func showError() {
-        let ac = UIAlertController(title: "Error", message: "There was a problem loading the page. Try again later", preferredStyle: .alert)
-        
-        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        
-        present(ac, animated: true )
+        DispatchQueue.main.async { [weak self] in
+            let ac = UIAlertController(title: "Error", message: "There was a problem loading the page. Try again later", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self?.present(ac, animated: true )
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
